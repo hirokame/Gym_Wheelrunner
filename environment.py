39496b4601b565bd36e940fafb2721aef54e10f9
hirokame <kou.hiro.1006@gmail.com>
2022-09-27 12:53:02 +0900

    Update environment.py

--- environment.py
+++ environment.py
@@ -21,9 +21,10 @@ class CustomEnv(gym.Env):
         
         self.dt = 0.01 # 10msごとの制御
         self.prepare = 0.3 # ペグが到着する何sec前にヒゲでdetectするか。
+        self.frame = int(self.prepare/self.dt) #ヒゲでdetectしてから何stepでペグが到着するか。
         
-        self.L_upcoming = []
-        self.R_upcoming = []
+        self.L_pegloc = []
+        self.R_pegloc = []
         
     def reset(self):
         '''
@@ -39,25 +40,25 @@ class CustomEnv(gym.Env):
         self.L_stimulus = 0
         self.R_stimulus = 0
         
-        self.L_detected = False
-        self.R_detected = False
+        self.L_pegloc = False
+        self.R_pegloc = False
         
-        obs = [self.L_ang, self.R_ang, self.L_angV, self.R_angV, self.L_stimulus, self.R_stimulus, self.L_detected, self.R_detected]
+        obs = [self.L_ang, self.R_ang, self.L_angV, self.R_angV, self.L_stimulus, self.R_stimulus, self.L_pegloc, self.R_pegloc]
         
         return obs
 
     def step(self, action):
         ### 左右の脚の角速度を更新
         if action["Left"] == 0: # TORQUE UP
-            self.L_angV += 0.1
+            self.L_angV += 1
         elif action["Left"] == 2 # TORQUE DOWN
-            self.L_angV -= 0.1
+            self.L_angV -= 1
             
         elif action["Right"] == 0: # TORQUE UP
-            self.R_angV += 0.1
+            self.R_angV += 1
         elif action["Right"] == 2: # TORQUE DOWN
-            self.R_angV -= 0.1
-        http://www.numpy.pi()/
+            self.R_angV -= 1
+
         ### 左右の脚の角度(位相)を更新
         dR_ang = self.dt * self.R_angV
         dL_ang = self.dt * self.L_angV
@@ -65,37 +66,45 @@ class CustomEnv(gym.Env):
         self.R_ang = (self.R_ang + dR_ang)%(np.pi*2)
         self.L_ang = (self.L_ang + dL_ang)%(np.pi*2)
         
-        ### Anticipationを更新
-        self.L_upcoming.remove(0)
-        self.R_upcoming.remove(0)
-        if len(self.L_upcoming) != 0:
-            for i in range(len(self.L_upcoming)):
-                self.L_upcoming[i] -= 1
-        if len(self.R_upcoming) != 0:
-            for i in range(len(self.R_upcoming)):
-                self.R_upcoming[i] -= 1
-                
-        self.L_stimulus -= self.gain * len(self.L_upcoming)
-        self.R_stimulus -= self.gain * len(self.R_upcoming)
+        ### Stimulationを更新
+        self.L_pegloc.remove(-10) ## ペグが到着してから10frame過ぎたらpeglocから消去する
+        self.R_pegloc.remove(-10)
+        if len(self.L_pegloc) != 0:
+            for i in range(len(self.L_pegloc)):
+                self.L_pegloc[i] -= 1
+        if len(self.R_pegloc) != 0:
+            for i in range(len(self.R_pegloc)):
+                self.R_pegloc[i] -= 1
+        
+        L_upcoming = len(self.L_pegloc[np.where(self.pegloc)])
+        self.L_stimulus -= self.gain * L_upcoming
+        self.R_stimulus -= self.gain * R_upcoming
         
         if self.L_detected:
-            self.L_upcoming.append(10)
+            self.L_pegloc.append(self.frame)
             self.L_stimulus += 1
         elif self.R_detected:
-            self.R_upcoming.append(10)
+            self.R_pegloc.append(self.frame)
             self.R_stimulus += 1
         
         obs = obs = [self.L_ang, self.R_ang, self.L_angV, self.R_angV, self.L_stimulus, self.R_stumulus]
         
         """
-        intshift: 
+        int_value  : 周期の変化が少ない場合(ACTIONがNONE、つまり角加速度が0の場合)報酬をもらえる。
+        phase_value: 位相の変化が少ない場合(右と左脚のAngular Velocityの差が小さい)報酬をもらえる。(angular velocityの逆数で報酬を与える)
+        failed     : ペグが来ている場合に
         """
         if action["Left"] == 1: # NONE
-            intshift += 1
+            int_value += 0.5
         elif action["Right"] == 1: # NONE
-            intshift += 1
+            int_value += 0.5
+        phase_value = 1/(1 + (self.R_angV - self.L_angV)**2)
         
-        phaseshift = (self.R_angV - self.L_angV)**2
+        reward = int_value + 1/(1+phase_shift)
+        
+        if failed:
+            reward -= 50
+            done = True
         
         return obs, reward, done, info
 
